772bb0d344dbe103fd0b881ed679305d6dcd7c6e
kuangmincong <kuangmc1008@163.com>
2018-11-01 16:50:11 +0800

    update

--- index.js
+++ index.js
@@ -2,27 +2,19 @@ const path = require("path");
 const fs = require("fs");
 
 var tinify = require("tinify");
-tinify.key = "Configure your key";
-// 默认每个Key每月可压缩图片数为500
+tinify.key = "";
 let totalCount = 500;
+
 tinify.validate().then(() => {
 	let restCount = totalCount - tinify.compressionCount;
 	if (restCount <= 0) {
 		console.log("当前Key压缩次数已用完，请更换Key！");
 		return;
 	}
-	mapDirName("./img")
-		.then(() => {
-			console.log(
-				`压缩完成，当前Key剩余次数为${totalCount -
-					tinify.compressionCount}次`
-			);
-		})
-		.catch(error => {
-			console.log("图片压缩失败！");
-		});
+	console.log(`当前Key剩余次数为${totalCount - tinify.compressionCount}次`);
+	mapDirName("./assets");
 }).catch((error) => {
-	console.log('Key认证失败！');
+	console.log("Key认证失败！");
 });
 
 function mapDirName(dirName) {
@@ -37,14 +29,14 @@ function mapDirName(dirName) {
 		if (stats.isDirectory()) {
 			mapDirName(name);
 		} else {
-			return compressImg(name);
+			compressImg(name);
 		}
 	}
 }
 
 function compressImg(img) {
 	let source = tinify.fromFile(img);
-	let reg = /.*(\d|a-z|A-Z)+\.png$/;
+	let reg = /.*\/(.*)\.png$/;
 	let result = null;
 	if ((result = reg.exec(img))) {
 		// let rename = img.replace(result[1], "__" + result[1]);
