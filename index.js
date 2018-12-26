988d13f9e03dca5538c134717523419a40dce510
kuangmincong <kuangmc1008@163.com>
2018-12-26 15:11:16 +0800

    添加进度提示

--- index.js
+++ index.js
@@ -1,66 +1,83 @@
 const path = require("path");
 const fs = require("fs");
-
-var tinify = require("tinify");
+const readline = require("readline");
+const tinify = require("tinify");
 
 // 填写 tingpng官网注册的 key
 tinify.key = "";
 
 //默认每月可压缩图片数为500
-let totalCount = 500;
+const totalCount = 500;
+
+// 需要压缩的图片数组
+const imgArray = [];
+
+// 当前进度
+let step = 0;
+
+// 图片所在目录,默认为img
+const imgdir = "./img";
 
 tinify
 	.validate()
 	.then(() => {
 		let restCount = totalCount - tinify.compressionCount;
 		if (restCount <= 0) {
-			console.log("当前Key压缩次数已用完，请更换Key！");
+			console.log("\n当前Key压缩次数已用完，请更换Key！");
 			return;
 		}
 		console.log(
-			`当前Key剩余次数为${totalCount - tinify.compressionCount}次`
+			`\n当前Key剩余次数为${totalCount - tinify.compressionCount}次`
 		);
-		mapDirName("./img");
+		mapDirName(imgdir);
+		imgArray.map(img => compressImg(img));
 	})
 	.catch(error => {
-		console.log("Key认证失败，请重新检查填写！");
+		console.log("\nKey认证失败，请重新检查填写！");
 	});
 
 function mapDirName(dirName) {
+	let reg = /.*\/(.*)\.png$/;
 	const imgFiles = fs.readdirSync(path.resolve(__dirname, dirName));
 	if (!imgFiles || imgFiles.length === 0) {
 		return;
 	}
-	console.log("正在压缩，请等待...");
 	for (let i = 0; i < imgFiles.length; i++) {
 		let name = `${dirName}/${imgFiles[i]}`;
 		let stats = fs.statSync(name);
 		if (stats.isDirectory()) {
 			mapDirName(name);
-		} else {
-			compressImg(name);
+		} else if (reg.exec(name)) {
+			imgArray.push(name);
 		}
 	}
 }
 
 function compressImg(img) {
-	let reg = /.*\/(.*)\.png$/;
-	let result = null;
-	if ((result = reg.exec(img))) {
-		// let rename = img.replace(result[1], "__" + result[1]);
-		let source = tinify.fromFile(img);
-		return source.toFile(img).catch(err => {
+	let source = tinify.fromFile(img);
+	return source
+		.toFile(img)
+		.then(() => {
+			step++;
+			let currStep = Math.floor((step / imgArray.length) * 100);
+			readline.clearLine(process.stdout, 0);
+			readline.cursorTo(process.stdout, 0, 3);
+			process.stdout.write(`正在压缩，当前进度为：${currStep}%`);
+			if (currStep === 100) {
+				console.log("\n\n压缩完成！");
+			}
+		})
+		.catch(err => {
 			if (err instanceof tinify.AccountError) {
-				console.log("当前Key的剩余次数可能不足：" + err.message);
+				console.log("\n当前Key的剩余次数不足：" + err.message);
 			} else if (err instanceof tinify.ClientError) {
-				console.log("客户端发生错误，请稍后重试！");
+				console.log("\n客户端发生错误，请稍后重试！");
 			} else if (err instanceof tinify.ServerError) {
-				console.log("服务端发生错误，请稍后重试！");
+				console.log("\n服务端发生错误，请稍后重试！");
 			} else if (err instanceof tinify.ConnectionError) {
-				console.log("连接失败，请稍后重试！");
+				console.log("\n连接失败，请稍后重试！");
 			} else {
-				console.log("发生未知错误！");
+				console.log("\n发生未知错误！");
 			}
 		});
-	}
 }
